00a9c21fb4e77488094ddc7b82a57c8d975e8c5d
carolinabsts <carolina.bustos@dinocloudconsulting.com>
2020-11-18 11:24:29 -0300

    Import ToDoListPage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ToDoListPage from './components/ToDoListPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ¡Welcome to React 101!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ToDoListPage/>
 }
 
 export default App;
